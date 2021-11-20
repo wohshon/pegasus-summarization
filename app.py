5fffe8c30fae95e44efacb106d1d6b95e1914f1f
Woh Shon Phoon <wohshon@google.com>
2021-11-20 16:13:45 +0000

    preload model, spilt into 2 models per service

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
 from flask import Flask, request, jsonify
 import torch
-from transformers import PegasusTokenizer, PegasusForConditionalGeneration
-model_name = 'google/pegasus-xsum'
+from transformers import PegasusTokenizer, PegasusForConditionalGeneration, PegasusConfig
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-tokenizer = PegasusTokenizer.from_pretrained(model_name)
-model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
 
+# modelNames = ['google/pegasus-xsum','google/pegasus-multi_news','google/pegasus-cnn_dailymail','google/pegasus-newsroom','google/pegasus-wikihow']
+# break up to 2 models per cloud run service due to the size of the models
+# summarize-1 tag :xsum-multi_news
+# modelNames = ['google/pegasus-xsum','google/pegasus-multi_news']
+# summarize-2, tag :cnn_dailymail-newsroom
+modelNames = ['google/pegasus-cnn_dailymail','google/pegasus-newsroom']
+
+# preload
+models =[]
+tokenizers = []
+print(' preloading models.... will take a while')
+for m in modelNames:
+    print('#'+str(modelNames.index(m)))
+    print('pre-loading '+m)
+    tokenizer = PegasusTokenizer.from_pretrained(m)
+    model = PegasusForConditionalGeneration.from_pretrained(m).to(device)
+    models.append(model)
+    tokenizers.append(tokenizer)
+print('....done')
 app = Flask(__name__)
 @app.route('/summarize', methods=['GET', 'POST'])
 def test():
     content = request.json
     print(content['text_payload'])
+    print(content['model'])    
+    model_name = content['model']
+    model_indx = modelNames.index(model_name)
+    print('index: '+str(model_indx))
+    # tokenizer = PegasusTokenizer.from_pretrained(model_name)
+    # model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
+    tokenizer = tokenizers[model_indx]
+    model = models[model_indx]
     batch = tokenizer(content['text_payload'], truncation=True, padding='longest', return_tensors="pt").to(device)
     translated = model.generate(**batch)
     tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
